7cc0f1135b80242943cbccfbd571d4a46e57feb9
swastiknepal-hash <anwarmdshahid2@gmail.com>
2025-06-12 10:39:20 +0545

    Update contact-form.php

--- contact-form.php
+++ contact-form.php
@@ -1,12 +1,1 @@
-<form action="https://formsubmit.co/swastiksoftware12@gmail.com" method="POST">
-  <input type="text" name="name" placeholder="Your Name" required>
-  <input type="email" name="email" placeholder="Email address" required>
-  <input type="text" name="phone" placeholder="Mobile Number">
-  <input type="text" name="company" placeholder="Company Name">
-  <textarea name="message" placeholder="Your Message" required></textarea>
-  
-  <!-- Optional: Redirect to thank you page -->
-  <input type="hidden" name="_next" value="https://swastiknepal-hash.github.io/swastik/thankyou.html">
-  
-  <button type="submit">Send Message</button>
-</form>
+
